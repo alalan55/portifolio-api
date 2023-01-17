ce31774694f46fe840b4104fc199f83331bbdcab
alalan55 <alla.ao4975@gmail.com>
2023-01-17 20:18:16 -0300

    Inicia construção da classe de autenticação

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+
+class createUser(BaseModel):
+    name: str
+    email: str
+    password: str
+    about: Optional[str]
+    profile_pic: Optional[str]
+
 
 @app.get("/")
 async def health():
