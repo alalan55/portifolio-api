024df95d1511cfd3c1ed27c9980d58edcd9e11d8
alalan55 <alla.ao4975@gmail.com>
2023-01-18 19:41:49 -0300

    Insere criação e listagem de experiencias

--- main.py
+++ main.py
@@ -51,6 +51,15 @@ class Project(BaseModel):
         gt=-1, lt=2, description="O estado do projeto deve estar entre 0 e 1")
 
 
+class Experience(BaseModel):
+    name_company: str
+    start_at: str
+    end_at: str
+    description: str
+    role: str
+    is_my_current_work: bool
+
+
 def get_db():
     try:
         db = SessionLocal()
@@ -142,8 +151,33 @@ async def login(user: UserLogin, db: Session = Depends(get_db)):
 
 # AÇÕES DAS EXPERIÊNCIAS ------------------------------------------------
 
+@app.get("/experiences")
+async def get_experices(db: Session = Depends(get_db)):
+    projects = db.query(models.Experiences).all()
+    return successful_response(200, projects)
 
-# AÇÕES DOS PROJETOS ------------------------------------------------
+
+@app.post("/experience")
+async def create_experience(experience: Experience, db: Session = Depends(get_db), user: dict = Depends(get_current_user)):
+    if user is None:
+        raise get_user_exception()
+
+    exp_model = models.Experiences()
+
+    exp_model.name_company = experience.name_company
+    exp_model.start_at = experience.start_at
+    exp_model.end_at = experience.end_at
+    exp_model.description = experience.description
+    exp_model.role = experience.role
+    exp_model.is_my_current_work = experience.is_my_current_work
+
+    db.add(exp_model)
+    db.commit()
+
+    return successful_response(201, experience)
+
+    # AÇÕES DOS PROJETOS ------------------------------------------------
+
 
 @app.get("/projects")
 async def get_projects(db: Session = Depends(get_db)):
