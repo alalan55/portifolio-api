46af5bacdebb693af605e44fd21bdfbb9fd414cc
alalan55 <alla.ao4975@gmail.com>
2023-01-21 14:34:22 -0300

    Insere link para adicionar curso

--- routes/courses.py
+++ routes/courses.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
+from typing import Optional
 from routes.auth import get_current_user, get_user_exception
 from database import engine, SessionLocal
 import models
@@ -17,6 +18,16 @@ router = APIRouter(
 )
 
 
+class CreateCourse(BaseModel):
+    name: str
+    description: str
+    start_at: str
+    end_at: str
+    pic: str
+    institution: str
+    link: str
+
+
 def get_db():
     try:
         db = SessionLocal()
@@ -29,3 +40,33 @@ def get_db():
 async def get_all_courses(db: Session = Depends(get_db)):
     courses = db.query(models.Courses).all()
     return courses
+
+
+@router.post("/")
+async def create_course(course: CreateCourse, db: Session = Depends(get_db), user: dict = Depends(get_current_user)):
+    if user is None:
+        raise get_user_exception()
+
+    course_model = models.Courses()
+
+    course_model.name = course.name
+    course_model.description = course.description
+    course_model.start_at = course.start_at
+    course_model.end_at = course.end_at
+    course_model.pic = course.pic
+    course_model.institution = course.institution
+    course_model.link = course.link
+    course_model.owner_id = user.get("id")
+
+    db.add(course_model)
+    db.commit()
+
+    return successful_response(201, course)
+
+
+def successful_response(status_code: int, content: Optional[dict or list] = None):
+    return {
+        "status": status_code,
+        "message": "Sucesso!",
+        "content": content
+    }
